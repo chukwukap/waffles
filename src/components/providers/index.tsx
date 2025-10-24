5cf8b3a0e88105d7855ca138ca826505217a2e48
Chukwuka Uba <promiseuba67@gmail.com>
2025-10-24 08:39:10 +0100

    remove device gate

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,19 +2,19 @@ import { MinikitProvider } from "./MinikitProvider";
 import { GameProvider } from "./GameProvider";
 import GlobalToaster from "../ui/Toaster";
 import { OnboardingGate } from "../onboarding/onboarding-gate";
-import DeviceGate from "../DeviceGate";
+// import DeviceGate from "../DeviceGate";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <DeviceGate continueAnyway={process.env.NODE_ENV == "production"}>
-      <MinikitProvider>
-        <OnboardingGate>
-          <GameProvider>
-            {children}
-            <GlobalToaster />
-          </GameProvider>
-        </OnboardingGate>
-      </MinikitProvider>
-    </DeviceGate>
+    // <DeviceGate continueAnyway={process.env.NODE_ENV == "production"}>
+    <MinikitProvider>
+      <OnboardingGate>
+        <GameProvider>
+          {children}
+          <GlobalToaster />
+        </GameProvider>
+      </OnboardingGate>
+    </MinikitProvider>
+    // </DeviceGate>
   );
 }
