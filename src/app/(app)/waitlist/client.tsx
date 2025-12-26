e4afdac80d0ebfc9b4f43a0b5f8c2e0673638d3d
Chukwuka Uba <promiseuba67@gmail.com>
2025-12-26 09:20:14 +0100

    remove console log from waitlist

--- src/app/(app)/waitlist/client.tsx
+++ src/app/(app)/waitlist/client.tsx
@@ -412,7 +412,6 @@ function JoinCTA({
 // ============================================
 export function WaitlistClient() {
   const { context } = useMiniKit();
-  console.log(JSON.stringify(context, null, 2));
   const addFrame = useAddFrame();
   const searchParams = useSearchParams();
   const { composeCastAsync } = useComposeCast();
