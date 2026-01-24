0725f48f4c2ede2c9cb9aa1090a4a60832916ec2
Chukwuka Uba <promiseuba67@gmail.com>
2026-01-24 17:25:06 +0100

    fix: re-add ExclamationCircleIcon import

--- src/components/admin/NotificationForm.tsx
+++ src/components/admin/NotificationForm.tsx
@@ -6,6 +6,7 @@ import {
     PaperAirplaneIcon,
     CheckCircleIcon,
     ChatBubbleLeftIcon,
+    ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
 import {
     sendAdminNotificationAction,
